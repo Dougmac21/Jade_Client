409504df8881874851d918312674edfe82ae66e9
Amy Browne <amybrowne@users-MacBook-Air.local>
2020-12-12 18:07:15 +0000

    added navbar items to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import MainContainer from './containers/MainContainer';
-import GamesContainer from './containers/GamesContainer'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
+import MainContainer from './containers/MainContainer';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GamesContainer from './containers/GamesContainer';
 
 
 
@@ -10,17 +10,17 @@ import Header from './components/Header';
 
 function App() {
   return (
+    <Router>
+      <>
+        <Header />
+        <Switch>
+        <Route exact path="/" component={MainContainer} />
+        <Route path="/games" component={GamesContainer} />
+        </Switch>
+      </>
+    </Router>
     
-  <Router>
-    <>
-      <Header />
-      <Switch>
-      <Route exact path="/" component={MainContainer} />
-      <Route path="/games" component={GamesContainer} />
-      {/* <Route component={ErrorPage}/> */}
-      </Switch>
-    </>
-  </Router>
+  
 
     // <div className="App">
     //   <h1>This is App.js</h1>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,16 +2,14 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ArcadeContainer from './ArcadeContainer';
 
+
 function MainContainer() {
     
-
-    return(
-        <>
-            <h2>This is the MainContainer</h2>
-            {/* <Header/> */}
-            <ArcadeContainer />
-            <Footer />
-        </>
+    return (
+    <>
+        <ArcadeContainer />
+        <Footer />
+    </>
     )
 };
 

6a5fafc21d436e8a5c78baa4e62bf008cadc55bf
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-13 21:33:52 +0000

    Almost working on player select dropdown. bedtime.

--- src/components/PlayerController.js
+++ src/components/PlayerController.js
@@ -7,33 +7,40 @@ function PlayerController() {
     const formRef = useRef(null);
     let registeredPlayersList = [];
     let registeredPasswordsList = [];
-
-
-
+    let dropdownPlayersList = [{label: '', value: '(select player)'}];
+       
+    
     function fetchData() {
         fetch(url)
         .then(res => res.json())
         .then(data => {
             const registeredPlayers = data.map(({name}) => name);
-            console.log(registeredPlayers);
+            // console.log(registeredPlayers);
+            registeredPlayersList = registeredPlayers;
+            // console.log(registeredPlayersList);
 
             const registeredPasswords = data.map(({password}) => password);
-            console.log(registeredPasswords);
-
-            registeredPlayersList = registeredPlayers;
-            console.log(registeredPlayersList);
-
+            // console.log(registeredPasswords);
             registeredPasswordsList = registeredPasswords;
-            console.log(registeredPasswordsList);
-        })
-    };
+            // console.log(registeredPasswordsList);
+
+            console.log(dropdownPlayersList);
+
+            registeredPlayers.forEach(function(player){
+                dropdownPlayersList.push({ label: player, value: player })
+            });
+
+            console.log(dropdownPlayersList);
+    })
+}
+
 
 
     async function handleNameSubmit(event) {
         event.preventDefault();
         const { name } = event.target.elements
         for (let anyName of registeredPlayersList){
-            console.log(anyName);
+            // console.log(anyName);
             if (name.value === anyName){
                 alert("Name is already registered. Please select another name.")
                 formRef.current.reset()
@@ -61,14 +68,13 @@ function PlayerController() {
         <>
             <form onSubmit={handleNameSubmit} ref={formRef}>
                 <label for="select-player">Select Player:</label>
-                <select name="select-player" id="select-player">
-                    <option value="testName"></option>
-                    <option value="testName2"></option>
+                <select name="select-player" id="select-player"
+                    options={ dropdownPlayersList.map(({player}) => ({ value: player, label: player})) }>                
                 </select>
 
                 <p>Register as a player:</p>
                 <input
-                    type="text" placeholder="username" name="name">
+                    required type="text" placeholder="username" name="name">
                 </input>
                 <input
                     type="submit" value="Submit" >

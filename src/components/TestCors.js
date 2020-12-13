d8c302deebeaa7e2fccacbde158201214b3de584
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-13 15:21:04 +0000

    Adding filter on username

--- src/components/TestCors.js
+++ src/components/TestCors.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from 'react';
 
-const url = "http://localhost:8080/players"
 
 function TestCors() {
+
+    const url = "http://localhost:8080/players";
+
+    let registeredPlayers = [];
+
+    function fetchRegisteredPlayers() {
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data.name)
+            })
+    };
+
     async function handleNameSubmit(event) {
-        console.log("test")
         event.preventDefault();
         const { name } = event.target.elements
-        debugger
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -15,27 +25,28 @@ function TestCors() {
         });
         // if (response){
         // }
-    }
+    };
 
 
     useEffect(() => {
         fetchData()
+
     }, [])
 
     function fetchData() {
-
         fetch(url)
             .then(res => res.json())
             .then(data => console.log(data))
-    }
+    };
 
 
 
     return (
         <>
             <form onSubmit={handleNameSubmit}>
+                <p>Register as a player:</p>
                 <input
-                    type="text" placeholder="Insert name here" name="name">
+                    type="text" placeholder="username" name="name">
                 </input>
                 <input
                     type="submit" value="Submit" >

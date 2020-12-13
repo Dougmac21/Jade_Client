07e1a16dd64fd69da14f79b4901f625f0b350e3b
emmikoivisto <emetsi@gmail.com>
2020-12-13 15:19:43 +0000

    tried to create a grid on games container

--- src/containers/GamesContainer.js
+++ src/containers/GamesContainer.js
@@ -1,17 +1,20 @@
 import Breakout from '../components/Breakout';
 import PersonalBest from '../components/PersonalBest';
 import SuggestedGames from '../components/SuggestedGames';
+import './GamesContainer.css'
 
 function GamesContainer() {
 
 
 
-    return(
+    return (
         <>
-            <h2>This is the GamesContainer</h2>
-            <Breakout />
-            <PersonalBest />
-            <SuggestedGames />
+            <div className="games-container">
+                <h2>This is the GamesContainer</h2>
+                <Breakout className="breakout" />
+                <PersonalBest className="personal-best" />
+                <SuggestedGames className="suggested-games" />
+            </div>
         </>
     )
 };

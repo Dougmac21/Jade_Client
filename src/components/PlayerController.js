033690d3a6c1a63a793c7902e4bd54b0b196ce8d
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-13 20:06:58 +0000

    Adding in psuso-authentication

--- src/components/PlayerController.js
+++ src/components/PlayerController.js
@@ -1,12 +1,33 @@
 import { useEffect, useRef } from 'react';
 
 
-function TestCors() {
+function PlayerController() {
 
     const url = "http://localhost:8080/players";
     const formRef = useRef(null);
-
     let registeredPlayersList = [];
+    let registeredPasswordsList = [];
+
+
+
+    function fetchData() {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const registeredPlayers = data.map(({name}) => name);
+            console.log(registeredPlayers);
+
+            const registeredPasswords = data.map(({password}) => password);
+            console.log(registeredPasswords);
+
+            registeredPlayersList = registeredPlayers;
+            console.log(registeredPlayersList);
+
+            registeredPasswordsList = registeredPasswords;
+            console.log(registeredPasswordsList);
+        })
+    };
+
 
     async function handleNameSubmit(event) {
         event.preventDefault();
@@ -14,9 +35,9 @@ function TestCors() {
         for (let anyName of registeredPlayersList){
             console.log(anyName);
             if (name.value === anyName){
-                alert("Name is already registered. Choose Again.")
+                alert("Name is already registered. Please select another name.")
                 formRef.current.reset()
-                return 
+                return
             }
         }
         const response = await fetch(url, {
@@ -35,25 +56,16 @@ function TestCors() {
         fetchData()
     }, [])
 
-    
-
-    function fetchData() {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            const registeredPlayers = data.map(({name}) => name);
-            // console.log(registeredPlayers)
-            registeredPlayersList = registeredPlayers;
-            // console.log(registeredPlayersList)
-        })
-    };
-
-
-
 
     return (
         <>
             <form onSubmit={handleNameSubmit} ref={formRef}>
+                <label for="select-player">Select Player:</label>
+                <select name="select-player" id="select-player">
+                    <option value="testName"></option>
+                    <option value="testName2"></option>
+                </select>
+
                 <p>Register as a player:</p>
                 <input
                     type="text" placeholder="username" name="name">
@@ -66,4 +78,4 @@ function TestCors() {
     )
 };
 
-export default TestCors;
+export default PlayerController;
--- src/containers/GamesContainer.js
+++ src/containers/GamesContainer.js
@@ -1,13 +1,11 @@
-import TestCors from '../components/TestCors';
+import PlayerController from '../components/PlayerController';
 
 function GamesContainer() {
-
-
 
     return(
         <>
             <h2>This is the GamesContainer</h2>
-            <TestCors />
+            <PlayerController />
         </>
     )
 };

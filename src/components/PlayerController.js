54bfadfe6825935091e6baaa193b7c38822224c1
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-14 10:15:48 +0000

    pre merging commit

--- src/components/PlayerController.js
+++ src/components/PlayerController.js
@@ -7,13 +7,24 @@ function PlayerController() {
     const formRef = useRef(null);
     let registeredPlayersList = [];
     let registeredPasswordsList = [];
-    let dropdownPlayersList = [{label: '', value: '(select player)'}];
+    let registeredPlayersObjectsList = [];
        
     
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
     function fetchData() {
         fetch(url)
         .then(res => res.json())
         .then(data => {
+            // console.log(data)
+            const registeredPlayersObjects = [].concat(data);
+            // console.log(registeredPlayersObjects)
+            registeredPlayersObjectsList = registeredPlayersObjects;
+            console.log(registeredPlayersObjectsList);
+
             const registeredPlayers = data.map(({name}) => name);
             // console.log(registeredPlayers);
             registeredPlayersList = registeredPlayers;
@@ -23,22 +34,26 @@ function PlayerController() {
             // console.log(registeredPasswords);
             registeredPasswordsList = registeredPasswords;
             // console.log(registeredPasswordsList);
-
-            console.log(dropdownPlayersList);
-
-            registeredPlayers.forEach(function(player){
-                dropdownPlayersList.push({ label: player, value: player })
-            });
-
-            console.log(dropdownPlayersList);
-    })
-}
-
+        })
+    }
 
 
     async function handleNameSubmit(event) {
         event.preventDefault();
         const { name } = event.target.elements
+        const { password } = event.target.elements
+
+        for (let anyName of registeredPlayersList){
+            for (let anyPassword of registeredPasswordsList){
+                if (name.value === anyName && password.value === anyPassword){
+                    alert(`Login Successful. Welcome back to the arcade ${name.value}.`)
+                    login(name.value)
+                    formRef.current.reset()
+                    return
+                }
+            }
+        }
+
         for (let anyName of registeredPlayersList){
             // console.log(anyName);
             if (name.value === anyName){
@@ -58,30 +73,30 @@ function PlayerController() {
         }
     };
 
+    function login(username){
+        return
+    }
 
-    useEffect(() => {
-        fetchData()
-    }, [])
 
 
     return (
         <>
             <form onSubmit={handleNameSubmit} ref={formRef}>
-                <label for="select-player">Select Player:</label>
-                <select name="select-player" id="select-player"
-                    options={ dropdownPlayersList.map(({player}) => ({ value: player, label: player})) }>                
-                </select>
 
-                <p>Register as a player:</p>
+                <p>Arcade Login</p>
                 <input
-                    required type="text" placeholder="username" name="name">
+                    required type="text" placeholder="username" name="name" id="name">
+                </input>
+                <input 
+                    required type="text" placeholder="password" name="password" id="password">
                 </input>
                 <input
-                    type="submit" value="Submit" >
+                    type="submit" value="Login" >
                 </input>
             </form>
         </>
     )
 };
+
 
 export default PlayerController;

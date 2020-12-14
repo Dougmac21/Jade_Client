22bd79e1b58ccd5f1966ef1bfe2ad76607606a73
emmikoivisto <emetsi@gmail.com>
2020-12-14 11:50:49 +0000

    flexbox added to list container

--- src/containers/GamesListContainer.js
+++ src/containers/GamesListContainer.js
@@ -1,12 +1,33 @@
+import './GamesListContainer.css'
 
 function GamesListContainer() {
 
-    
 
     return (
         <>
             <h2>This is the List of Playable GamesContainer</h2>
-            <ul>
+            <ul id="games-list">
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
+                <li> 
+                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                </li>
                 <li> 
                     <img src="https://placebear.com/300/300" alt="bear boi"></img>
                 </li>

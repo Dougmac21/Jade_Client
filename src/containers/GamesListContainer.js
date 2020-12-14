7d46d8fba8dde15b1a6c367014f19ab12f3acf37
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-14 14:48:12 +0000

    Rounded corners

--- src/containers/GamesListContainer.js
+++ src/containers/GamesListContainer.js
@@ -12,7 +12,7 @@ function GamesListContainer() {
 
 
                 <li class="games-list-item" id="game-1"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play Snake</div>
                     </div>
@@ -20,31 +20,31 @@ function GamesListContainer() {
 
 
                 <li class="games-list-item" id="game-2"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play Runner</div>
                     </div>
                 </li>
                 <li class="games-list-item" id="game-3"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play Breakout</div>
                     </div>
                 </li>
                 <li class="games-list-item" id="game-4"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play PRS</div>
                     </div>
                 </li>
                 <li class="games-list-item" id="game-5"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play Frogger</div>
                     </div>
                 </li>
                 <li class="games-list-item" id="game-6"> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <img class="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">Play Clicker</div>
                     </div>
--- src/containers/ScoreListContainer.js
+++ src/containers/ScoreListContainer.js
@@ -9,42 +9,42 @@ function ScoreListContainer() {
             <ul id="scores-list">
 
                 <li class="scores-list-item" id="scores-1">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>
                 </li>
 
                 <li class="scores-list-item" id="scores-2">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>
                 </li>
 
                 <li class="scores-list-item" id="scores-3">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>
                 </li>
 
                 <li class="scores-list-item" id="scores-4">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>
                 </li>
 
                 <li class="scores-list-item" id="scores-5">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>
                 </li>
 
                 <li class="scores-list-item" id="scores-6">
-                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                <img class="scores-list-img" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div class="overlay">
                         <div class="text">See All Scores</div>
                     </div>

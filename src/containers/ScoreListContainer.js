3a18e86740c552210d953fc03e2243b61f6c118b
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-14 14:40:20 +0000

    scorelist container css layout added

--- src/containers/ScoreListContainer.js
+++ src/containers/ScoreListContainer.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import './ScoreListContainer.css';
+
 
 function ScoreListContainer() {
 
     return (
-        <h2>Score list </h2>
+        <>
+            <ul id="scores-list">
+
+                <li class="scores-list-item" id="scores-1">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+                <li class="scores-list-item" id="scores-2">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+                <li class="scores-list-item" id="scores-3">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+                <li class="scores-list-item" id="scores-4">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+                <li class="scores-list-item" id="scores-5">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+                <li class="scores-list-item" id="scores-6">
+                <img src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="overlay">
+                        <div class="text">See All Scores</div>
+                    </div>
+                </li>
+
+            </ul>
+        </>
     )
 }
 

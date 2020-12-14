fe25fd2e27005a167cb9862b752e9ca6a7c9f983
emmikoivisto <emetsi@gmail.com>
2020-12-14 12:33:28 +0000

    adding css to games list

--- src/containers/GamesListContainer.js
+++ src/containers/GamesListContainer.js
@@ -3,28 +3,33 @@ import './GamesListContainer.css'
 function GamesListContainer() {
 
 
+
+
+
     return (
         <>
             <h2>This is the List of Playable GamesContainer</h2>
             <ul id="games-list">
-                <li> 
+
+
+                <li class="container" id="game-1"> 
+                    <div class="overlay">
+                    <img class="image" src="https://placebear.com/300/300" alt="bear boi"></img>
+                    <div class="text">Hello World</div>
+                    </div>
+                </li>
+
+
+                <li class="games-list-item" id="game-2"> 
                     <img src="https://placebear.com/300/300" alt="bear boi"></img>
                 </li>
-                <li> 
+                <li class="games-list-item" id="game-3"> 
                     <img src="https://placebear.com/300/300" alt="bear boi"></img>
                 </li>
-                <li> 
+                <li class="games-list-item" id="game-4"> 
                     <img src="https://placebear.com/300/300" alt="bear boi"></img>
                 </li>
-                <li> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
-                </li>
-                <li> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
-                </li>
-                <li> 
-                    <img src="https://placebear.com/300/300" alt="bear boi"></img>
-                </li>
+
             </ul>
         </>
     )

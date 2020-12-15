3265838228c1a9ca691e30fb698d28ee7624383f
emmikoivisto <emetsi@gmail.com>
2020-12-15 11:39:49 +0000

    snake game route added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import HomePage from './components/HomePage';
 import GamesListContainer from './containers/GamesListContainer';
 import ScoreListContainer from './containers/ScoreListContainer';
 import PlayerController from './components/PlayerController';
-import Snake from './components/games/Snake';
+import SnakeGame from './components/games/SnakeGame';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/games" component={GamesListContainer} />
           <Route path="/scores" component={ScoreListContainer} />
           <Route path="/log-in" component={PlayerController} />
-          <Route path="/games/snake" component={Snake} />
+          <Route path="/games/snake" component={SnakeGame} />
         </>
       </Switch>
     </Router>

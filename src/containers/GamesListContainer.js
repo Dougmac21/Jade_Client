b0ed382d445299b7ce92533942af899885b873b5
emmikoivisto <emetsi@gmail.com>
2020-12-16 17:29:59 +0000

    paper rock scissors image added

--- src/containers/GamesListContainer.js
+++ src/containers/GamesListContainer.js
@@ -14,15 +14,14 @@ function GamesListContainer() {
 
                 <li className="games-list-item" id="game-1">
                     <div >
-                        <a href="http://localhost:3000/games/snake" className="game-list-overlay">Play Snake</a>
+                        <a href="http://localhost:3000/games/snake" className="game-list-overlay">Play Snake!</a>
                     </div>
                 </li>
 
 
                 <li className="games-list-item" id="game-2">
-                    <img className="games-list-image" src="https://placebear.com/300/300" alt="bear boi"></img>
                     <div >
-                        <a href="http://localhost:3000/games/PRS" className="game-list-overlay">Play PRS</a>
+                        <a href="http://localhost:3000/games/PRS" className="game-list-overlay">Play Paper Rock Scissors!</a>
                     </div>
                 </li>
                 <li className="games-list-item" id="game-3">

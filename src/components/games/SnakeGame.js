76c38471fcc1402ddaa16564477459330bf281b4
JenkenK <jenkenkwok@gmail.com>
2020-12-16 09:54:22 +0000

    replaced snake game code with malcolm's help

--- src/components/games/SnakeGame.js
+++ src/components/games/SnakeGame.js
@@ -1,232 +1,269 @@
-// import Snake from 'react-simple-snake'
-// import Header from '../Header';
-// import Footer from '../Footer';
-
-// export default function SnakeGame() {
-//     return (
-//         <div>
-//             <Header />
-//             <Snake /> 
-//             <Footer />
-//         </div>
-//     )
-// }
-
-import Header from '../Header';
-import Footer from '../Footer';
-import '../../styles/Snake.css'
-
-document.addEventListener('DOMContentLoaded', () => {
-    const squares = document.querySelectorAll('.grid div')
-    const scoreDisplay = document.querySelector('span')
-    const startBtn = document.querySelector('.start')
-
-    const width = 10
-    let currentIndex = 0 //so first div in our grid
-    let appleIndex = 0 //so first div in our grid
-    let currentSnake = [2, 1, 0]
-    let direction = 1
-    let score = 0
-    let speed = 0.9
-    let intervalTime = 0
-    let interval = 0
+import { useEffect, useState, useRef, useCallback } from 'react';
+import Header from '../Header'
+import Footer from '../Footer'
+import '../../styles/Snake.css';
 
 
-    //to start, and restart the game
-    function startGame() {
-        currentSnake.forEach(index => squares[index].classList.remove('snake'))
-        squares[appleIndex].classList.remove('apple')
-        clearInterval(interval)
-        score = 0
-        randomApple()
-        direction = 1
-        scoreDisplay.innerText = score
-        intervalTime = 200
-        currentSnake = [2, 1, 0]
-        currentIndex = 0
-        currentSnake.forEach(index => squares[index].classList.add('snake'))
-        interval = setInterval(moveOutcomes, intervalTime)
-    }
-
-
-    //function that deals with ALL the ove outcomes of the Snake
-    function moveOutcomes() {
-
-        //deals with snake hitting border and snake hitting self
-        if (
-            (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits bottom
-            (currentSnake[0] % width === width - 1 && direction === 1) || //if snake hits right wall
-            (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
-            (currentSnake[0] - width < 0 && direction === -width) ||  //if snake hits the top
-            squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
-        ) {
-            alert("You Lose")
-
-            return clearInterval(interval) //this will clear the interval if any of the above happen
+function SnakeGame() {
+    const [dim, setDim] = useState(0);
+    const [chunk, setChunk] = useState(0);
+    const [direction, setDirection] = useState('');
+    const [fruit, setFruit] = useState(26);
+    const [points, setPoints] = useState(0);
+    const [game, setGame] = useState(true);
+    const [gameOver, setGameOver] = useState(false)
+    const speedRef = useRef(100);
+    let width;
+    const [snake, setSnake] = useState([
+        {
+            direction: 'right',
+            part: [186, 185, 184, 183]
         }
+    ]);
 
-        const tail = currentSnake.pop() //removes last ite of the array and shows it
-        squares[tail].classList.remove('snake')  //removes class of snake from the TAIL
-        currentSnake.unshift(currentSnake[0] + direction) //gives direction to the head of the array
-
-        //deals with snake getting apple
-        if (squares[currentSnake[0]].classList.contains('apple')) {
-            squares[currentSnake[0]].classList.remove('apple')
-            squares[tail].classList.add('snake')
-            currentSnake.push(tail)
-            randomApple()
-            score++
-            scoreDisplay.textContent = score
-            clearInterval(interval)
-            intervalTime = intervalTime * speed
-            interval = setInterval(moveOutcomes, intervalTime)
+    const reset = () => {
+        speedRef.current = 100;
+        setPoints(0)
+        setDirection('right')
+        setSnake([{
+            direction: 'right',
+            part: [186, 185, 184, 183]
+        }])
+        setGame(false)
+        setGameOver(false)
+    }
+    const start = () => {
+        speedRef.current = 100;
+        setPoints(0)
+        setDirection('right')
+        setSnake([{
+            direction: 'right',
+            part: [186, 185, 184, 183]
+        }])
+        setGame(false)
+    }
+    const pieces = () => {//functionally label snake pieces (bang) and return
+        let arr = [];
+        for (let i = 0; i < 400; i++) {
+            let addToArr = false;
+            let j = 0;
+            while (j < snake.length) {
+                if (snake[j].part.indexOf(i) >= 0) {
+                    addToArr = true
+                    break;
+                } else {
+                    addToArr = false
+                }
+                j++
+            }
+            addToArr ? arr.push('bang') : i === fruit ? arr.push('fruit') : arr.push('')
         }
-        squares[currentSnake[0]].classList.add('snake')
+        return arr
     }
-
-
-    //generate new apple once apple is eaten
-    function randomApple() {
-        do {
-            appleIndex = Math.floor(Math.random() * squares.length)
-        } while (squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
-        squares[appleIndex].classList.add('apple')
-    }
-
-
-    //assign functions to keycodes
-    function control(e) {
-        squares[currentIndex].classList.remove('snake')
-
-        if (e.keyCode === 39) {
-            direction = 1 //if we press the right arrow on our keyboard, the snake will go right one
-        } else if (e.keyCode === 38) {
-            direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up
-        } else if (e.keyCode === 37) {
-            direction = -1 // if we press left, the snake will go left one div
-        } else if (e.keyCode === 40) {
-            direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
+    //handle direction changes
+    const turn = useCallback(
+        (dir, opp) => {
+            let tempSnake = [...snake];
+            console.log(snake[0].part)
+            if (snake[0].part.length > 0 && direction !== opp && direction !== dir) {
+                setDirection(dir)
+                tempSnake.unshift({
+                    direction: dir,
+                    part: []
+                })
+                setSnake(tempSnake)
+            }
+        }, [snake, direction]
+    )
+    useEffect(() => {
+        //determine relative dimensions of game portal
+        width = window.innerWidth;
+        if (width >= 800) {
+            setDim(width * .35)
+        } else if (width < 800) {
+            setDim(width * .9);
         }
-    }
-
-    document.addEventListener('keyup', control)
-    startBtn.addEventListener('click', startGame)
-})
-
-
-export default function SnakeGame() {
-
+        setChunk(dim / 20)
+        //points and get longer after eating
+        if (snake[0].part[0] === fruit) {
+            setPoints(points + 1)
+            let sneak = [...snake];
+            let firstSection = sneak[0]
+            if (firstSection.direction === 'up') {
+                let y = firstSection.part[0] - 20;
+                if (y < 0) {
+                    firstSection.part.unshift(y + 400);
+                } else {
+                    firstSection.part.unshift(y)
+                }
+            } else if (firstSection.direction === 'right') {
+                let y = firstSection.part[0] + 1;
+                if (y % 20 === 0) {
+                    firstSection.part.unshift(y + - 20);
+                } else {
+                    firstSection.part.unshift(y)
+                }
+            } else if (firstSection.direction === 'down') {
+                let y = firstSection.part[0] + 20;
+                if (y >= 400) {
+                    firstSection.part.unshift(y - 400);
+                } else {
+                    firstSection.part.unshift(y)
+                }
+            } else if (firstSection.direction === 'left') {
+                let y = firstSection.part[0] - 1;
+                if (y % 20 === 19) {
+                    firstSection.part.unshift(y + 20);
+                } else {
+                    firstSection.part.unshift(y)
+                }
+            }
+            speedRef.current = speedRef.current - 2
+            setSnake(sneak)
+            setFruit(Math.floor(Math.random() * Math.floor(400)))
+        }
+        //gameover if you eat your tail
+        let totalArr = [];
+        for (let k = 0; k < snake.length; k++) {
+            totalArr = [...totalArr, ...snake[k].part]
+        }
+        let head = snake[0].part[0];
+        totalArr.filter(item => item === head).length >= 2 && setGameOver(true)
+        if (!gameOver) {//if GAMEOVER pause events
+            //listen for directions and update snake instructions accordingly
+            const handleKeydown = (e) => {
+                //let tempSnake: any = [...snake];
+                switch (e.code) {
+                    case 'ArrowUp':
+                        e.preventDefault();
+                        turn('up', 'down')
+                        break;
+                    case 'ArrowRight':
+                        e.preventDefault();
+                        turn('right', 'left')
+                        break;
+                    case 'ArrowDown':
+                        e.preventDefault();
+                        turn('down', 'up')
+                        break;
+                    case 'ArrowLeft':
+                        e.preventDefault();
+                        turn('left', 'right')
+                        break;
+                    default:
+                }
+            }
+            document.addEventListener('keydown', handleKeydown)
+            //event interval
+            const interval = setInterval(() => {
+                //handle snake piece movement
+                let dupSneak = [...snake];
+                for (let i = (snake.length - 1); i > 0; i--) {//increment through current snake and reduce to head direction
+                    if (dupSneak[i].part.length !== 0) {
+                        let next = dupSneak[i - 1];
+                        let chunk = dupSneak[i].part.shift();
+                        next.part.push(chunk)
+                    } else {
+                        dupSneak.pop()
+                    }
+                }
+                let sneak = dupSneak;
+                sneak.map((section) => {
+                    if (section.direction === 'right') {
+                        section.part.map((x, i) => {
+                            let y = x + 1;
+                            if (y % 20 === 0) {
+                                return section.part[i] = y - 20;
+                            } else {
+                                return section.part[i] = y
+                            }
+                        })
+                    } else if (section.direction === 'up') {
+                        section.part.map((x, i) => {
+                            let y = x - 20;
+                            if (y < 0) {
+                                return section.part[i] = y + 400;
+                            } else {
+                                return section.part[i] = y
+                            }
+                        })
+                    } else if (section.direction === 'left') {
+                        section.part.map((x, i) => {
+                            let y = x - 1;
+                            if (y % 20 === 19) {
+                                return section.part[i] = y + 20;
+                            } else {
+                                return section.part[i] = y
+                            }
+                        })
+                    } else if (section.direction === 'down') {
+                        section.part.map((x, i) => {
+                            let y = x + 20;
+                            if (y >= 400) {
+                                return section.part[i] = y - 400;
+                            } else {
+                                return section.part[i] = y
+                            }
+                        })
+                    }
+                    return ''
+                })
+                setSnake(sneak)
+            }, speedRef.current);
+            //remove interval and listeners
+            return () => {
+                clearInterval(interval)
+                document.removeEventListener('keydown', handleKeydown)
+            };
+        }
+    }, [turn, width, dim, chunk, snake, direction, points, fruit, game])
 
 
     return (
         <>
             <Header />
-            <button className='start'>Start</button>
-            <div className='score'>Score:<span>0</span></div>
-
-            <div className='grid'>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="snake-container" id="snake-container">
+                <div
+                    className="game-border"
+                    style={{ width: dim, height: dim, backgroundColor: "#ebebeb" }}>
+                    {
+                        pieces().map((piece, i) => {
+                            return <div
+                                key={'piece' + i}
+                                style={piece === 'bang' ?
+                                    { width: chunk, height: chunk, backgroundColor: "#248ec2" } :
+                                    piece === 'fruit' ?
+                                        { width: chunk, height: chunk, backgroundColor: "#1d355e" } :
+                                        { width: chunk, height: chunk }}
+                            >
+                            </div>
+                        })
+                    }
+                    {
+                        gameOver && <div className="game-splash" style={{ height: dim }}>
+                            <h1>Game Over</h1>
+                            <h2>Your score was {points}</h2>
+                            <button>Save Score</button>
+                            <button onClick={() => reset()}>Try Again</button>
+                        </div>
+                    }
+                    {
+                        game && <div className="game-splash" style={{ height: dim }}>
+                            <h1>GOOD LUCK</h1>
+                            <h2>Rules: blah blah</h2>
+                            <button onClick={() => start()}>Start</button>
+                        </div>
+                    }
+                </div>
+                <div
+                    className="point-bar"
+                    style={{ width: dim }}
+                >
+                    <div className="points-text">Score: {points}</div>
+                </div>
             </div>
             <Footer />
         </>
     )
 }
+export default SnakeGame;

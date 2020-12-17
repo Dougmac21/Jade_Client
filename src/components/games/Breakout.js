0694c8ecf57d737e45a852637cbefb0a6345722d
Douglas MacIntyre <dougmac21@talk21.com>
2020-12-17 19:41:50 +0000

    Hook into scoring not possible due to security policies for iframes

--- src/components/games/Breakout.js
+++ src/components/games/Breakout.js
@@ -1,7 +1,52 @@
 import Header from '../Header';
 import Footer from '../Footer';
+import { useEffect, useState } from 'react';
 
 function Breakout() {
+
+
+    const scoreUrl = "http://localhost:8080/scores";
+    const playerUrl = "http://localhost:8080/players";
+
+    const [breakoutScores, setBreakoutScores] = useState([])
+    const [registeredPlayersList, setRegisteredPlayersList] = useState([])
+    const [registeredPasswordsList, setRegisteredPasswordsList] = useState([])
+    const [registeredPlayersObjectsList, setRegisteredPlayersObjectsList] = useState([])
+
+
+    useEffect(() => {
+        fetchScoreData()
+        fetchPlayerData()
+    }, [])
+
+
+    function fetchPlayerData() {
+        fetch(playerUrl)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setRegisteredPlayersObjectsList(data)
+                console.log(registeredPlayersObjectsList);
+
+                setRegisteredPlayersList(data.map(({ name }) => name))
+                console.log(registeredPlayersList);
+
+                setRegisteredPasswordsList(data.map(({ password }) => password))
+                console.log(registeredPasswordsList);
+            })
+    }
+
+
+    function fetchScoreData() {
+        fetch(scoreUrl + "?gamename=breakout")
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setBreakoutScores(data.map(({ score }) => score))
+                console.log(breakoutScores);
+            })
+    }
+
 
 
     return(
@@ -9,14 +54,17 @@ function Breakout() {
             <Header />
 
             <div align="center" className="breakout-container">
-                <iframe class="inline-frame-breakout"
+
+                <iframe
+                class="inline-frame-breakout"
+                id="breakout-frame"
                 title="Inline Frame Breakout"
                 width="540"
                 height="500"
                 src="https://lod-js-breakout.herokuapp.com/" >
                 </iframe>
-            </div>
 
+            </div>
             <Footer />
         </>
     )
@@ -25,8 +73,3 @@ function Breakout() {
 };
 
 export default Breakout;
-
-
-
-
-
